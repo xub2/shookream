d9fe008bb6f68939abe3c26e80207ec986631c4a
xuv2 <fish5478@naver.com>
2025-11-24 16:02:20 +0900

    11/24 주문조회시 해당 표 다 끌고 오도록 페치조인

--- src/main/java/kream/shookream/repository/OrderRepository.java
+++ src/main/java/kream/shookream/repository/OrderRepository.java
@@ -2,6 +2,14 @@ package kream.shookream.repository;
 
 import kream.shookream.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("select o from Order o join fetch o.orderTickets where o.id = :id")
+    Optional<Order> findWithOrderTicketsById(@Param("id") Long id);
+
 }
