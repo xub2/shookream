e6c4142245012ba3a846bb2471eeaede6156a23d
xuv2 <fish5478@naver.com>
2025-11-24 16:02:36 +0900

    11/24 비관적 락 방식으로 동시성 해결

--- src/main/java/kream/shookream/repository/EventRepository.java
+++ src/main/java/kream/shookream/repository/EventRepository.java
@@ -1,7 +1,18 @@
 package kream.shookream.repository;
 
+import jakarta.persistence.LockModeType;
 import kream.shookream.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    // 비관적 락 적용
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select e from Event e where e.id = :id")
+    Optional<Event> findWithPessimisticLockById(@Param("id") Long id); // SELECT FOR UPDATE 문
 }
